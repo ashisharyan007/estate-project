d2205a5b79eba9e3eb37eeabc0f64d51acc90157
Ashish Aryan <Arthasaryan@gmail.com>
2025-01-20 19:15:54 +0530

    Project Completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div
+    <div id="Header"
       className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
       style={{ backgroundImage: "url('/header_img.png')" }}
     >
